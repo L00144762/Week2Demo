19394cfed13a498cd3d3a459e8f4263631f609c1
unknown <L00144762@student.lyit.ie>
2019-03-11 11:46:26 +0000

    initial commit

--- Week2Demo.R
+++ Week2Demo.R
@@ -2,3 +2,15 @@ x <- 2
 x
 y <- 5
 y
+
+x <- 5
+class(x) #note telesense
+is.numeric(x) #check if x is numeric
+
+i <- 5L #need tp type L after number to store as interger
+is.integer(i)
+
+date1 <- as.Date("2019-03-08") #yy/mm/dd
+date1
+class(date1)
+as.numeric(date1)
